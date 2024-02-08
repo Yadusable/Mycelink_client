1bc655facc749f3067b6df999d5ad8b7c28fe48f
= <=>
2024-02-08 15:11:45 +0100

    fix minor issues in main

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use mycelink_lib_api::api::APIConnector;
+use mycelink_lib_api::db::db_connector::DBConnector;
+use tauri::async_runtime::block_on;
+use tokio::runtime::Runtime;
+
+mod cmd_endpoints;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -9,8 +15,16 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .manage(create_api_connector())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+async fn create_api_connector() -> APIConnector<()> {
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async{
+        APIConnector::<()>::new(DBConnector::new("./loremipsumdolorsitamat.sqlite".as_ref()).await.unwrap())
+    })
 }
